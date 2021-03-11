7d2f59b965844da1e942a2dad69bcc01ad103672
truong.vo <truong.vo@CPU08-TRUONGVO>
2021-03-11 17:06:15 +0700

    abcdef

---
+++ ConsoleApp1/ConsoleApp3/Dropdowbox.cs
@@ -0,0 +1,78 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections;
+//using System.Collections.Generic;
+
+namespace ConsoleApp3
+{
+    class Dropdowbox
+    {
+        IWebDriver m_driver;
+
+        [Test]
+        public void cssDeme()
+        {
+            m_driver = new ChromeDriver("D:\\auto\\chromedriver");
+            m_driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            //driver.Url = "http://demo.guru99.com/test/selenium-xpath.html";
+            m_driver.Url = "http://demo.guru99.com/test/newtours/register.php";
+            //m_driver.Manage().Window.Maximize();
+
+            //IWebElement Drop_1 = m_driver.FindElement(By.Name("country")); Drop_1.Clear();
+            Thread.Sleep(2000);
+
+            IWebElement Drop = m_driver.FindElement(By.Name("country"));
+            SelectElement oSelection = new SelectElement(Drop);
+
+            oSelection.SelectByText("ANDORRA");
+
+            // Using sleep command so that changes can be notice
+            Thread.Sleep(2000);
+
+            // Step 5: Select option 'Africa' now (Use selectByVisibleText)
+            //oSelection.SelectByIndex(2);
+            //Thread.Sleep(2000);
+
+            // Step 6: Print all the options for the selected drop down and select one option of your choice
+            // Get the size of the Select element
+            IList<IWebElement> oSize = oSelection.Options;
+
+            int iListSize = oSize.Count;
+            // Setting up the loop to print all the options
+            for (int i = 0; i < iListSize; i++)
+            {
+                // Storing the value of the option 
+                String sValue = oSelection.Options.ElementAt(i).Text;
+                // Printing the stored value
+                Console.WriteLine("Value of the Select item is : " + sValue);
+
+                // Putting a check on each option that if any of the option is equal to 'Africa" then select it 
+                if (sValue.Equals("ARUBA"))
+                {
+                    oSelection.SelectByIndex(i);
+                    break;
+                }
+
+            }
+
+            Thread.Sleep(2000);
+
+            m_driver.Close();
+
+            Console.WriteLine("cssDeme");
+
+            Program program = new Program();
+            program.Testing_2();
+
+        }
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp3/Dropdownbox_2.cs
@@ -0,0 +1,69 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ConsoleApp3
+{
+    class Dropdownbox_2
+    {
+        [Test]
+        public void Test()
+        {
+            // Create a new instance of the Firefox driver
+            IWebDriver driver = new ChromeDriver("D:\\auto\\chromedriver");
+
+            // Put an Implicit wait, this means that any search for elements on the page could take the time the implicit wait is set for before throwing exception
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            // Launch the URL
+            driver.Url = "http://demo.guru99.com/test/newtours/register.php";
+
+            // Step 3: Select 'Selenium Commands' Multiple select box ( Use Name locator to identify the element )
+            IWebElement drop_1 = driver.FindElement(By.Name("country"));
+            SelectElement oSelection = new SelectElement(drop_1);
+            //SelectElement oSelection = new SelectElement(driver.FindElement(By.Name("country")));
+
+            // Step 4: Select option 'Browser Commands'  and then deselect it (Use selectByIndex and deselectByIndex)
+            //oSelection.SelectByIndex(0);
+            //Thread.Sleep(2000);
+            //oSelection.DeselectByIndex(0);
+
+            // Step 5: Select option 'Navigation Commands'  and then deselect it (Use selectByVisibleText and deselectByVisibleText)
+            oSelection.SelectByText("ALGERIA");
+            Thread.Sleep(2000);
+
+            oSelection.DeselectByText("ALGERIA");
+
+            // Step 6: Print and select all the options for the selected Multiple selection list.
+            IList<IWebElement> oSize = oSelection.Options;
+            int iListSize = oSize.Count;
+
+            // Setting up the loop to print all the options
+            for (int i = 0; i < iListSize; i++)
+            {
+                // Storing the value of the option 
+                String sValue = oSelection.Options.ElementAt(i).Text;
+                // Printing the stored value
+                Console.WriteLine("Value of the Item is :" + sValue);
+                // Selecting all the elements one by one
+                oSelection.SelectByIndex(i);
+ 
+                Thread.Sleep(500);
+            }
+
+            // Step 7: Deselect all
+            oSelection.DeselectAll();
+
+            // Kill the browser
+            driver.Close();
+        }
+    }
+}
