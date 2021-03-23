3a978e9af0b2ec79d5c5afd487598af82be4c552
truong.vo <truong.vo@CPU08-TRUONGVO>
2021-03-23 15:04:07 +0700

    4567899

--- ConsoleApp1/ConsoleApp2/Class1.cs
+++ ConsoleApp1/ConsoleApp2/Class1.cs
@@ -18,12 +18,22 @@ namespace ConsoleApp2
         [Test]
         public void cssDeme()
         {
-            m_driver = new ChromeDriver("D:\\auto\\chromedriver");
+            m_driver = new ChromeDriver("D:\\auto\\chromedriver\\New folder");
             //driver.Url = "http://demo.guru99.com/test/selenium-xpath.html";
-            m_driver.Url = "http://demo.guru99.com/test/guru99home/";
+            m_driver.Url = "http://demo.guru99.com/test/delete_customer.php";
             //m_driver.Manage().Window.Maximize();
+            m_driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
 
-            m_driver.SwitchTo().Alert().Dismiss();
+            OpenQA.Selenium.Chrome.ChromeOptions chromeOptions = new OpenQA.Selenium.Chrome.ChromeOptions();
+            chromeOptions.AddAdditionalCapability("resolution", "1024x768", true);
+
+            Console.WriteLine("tin nhan:" + chromeOptions);
+            Thread.Sleep(5000);
+
+            ChromeOptions options = new ChromeOptions();
+            options.AddExcludedArgument("disable-popup-blocking");
+
+            //m_driver.SwitchTo().Alert().Dismiss();
             
 
             // Store locator values of email text box and sign up button		
@@ -45,6 +55,12 @@ namespace ConsoleApp2
             emailTextBox1.SendKeys("test123@gmail.com");
             Thread.Sleep(2000);
 
+            IWebElement signUpButton1 = m_driver.FindElement(By.Name("submit"));
+            signUpButton1.Submit();
+            Thread.Sleep(2000);
+            Click_on_image click_On_Image = new Click_on_image();
+            click_On_Image.Testing_1();
+
             string alerMessage = m_driver.SwitchTo().Alert().Text;
 
             Console.WriteLine("tin nhan:" + alerMessage);
@@ -53,11 +69,7 @@ namespace ConsoleApp2
             m_driver.SwitchTo().Alert().Accept();
 
 
-
-
-
-
-            //Click_on_image click_On_Image = new Click_on_image();
+           //Click_on_image click_On_Image = new Click_on_image();
             //click_On_Image.Testing_1();
 
             //driver.navigate().back();
