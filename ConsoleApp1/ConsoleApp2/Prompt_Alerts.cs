0cb56c4bdce90026a17f11b783ea19a21d98dbae
truong.vo <truong.vo@CPU08-TRUONGVO>
2021-03-29 10:57:54 +0700

    mmmmmm

---
+++ ConsoleApp1/ConsoleApp2/Prompt_Alerts.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Support;
+using System.Threading;
+using System.Text.RegularExpressions;
+
+
+namespace ConsoleApp2
+{
+    class Prompt_Alerts
+    {
+        IWebDriver driver;
+        //public object arguments;
+
+        [Test]
+         public void alert()
+        {
+            driver = new ChromeDriver("D:\\auto\\chromedriver\\New folder");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            driver.Navigate().GoToUrl("https://toolsqa.com/handling-alerts-using-selenium-webdriver/");
+
+            IWebElement ele = driver.FindElement(By.XPath("//*[@id='promtButton']"));
+
+            //ele.Click();
+
+            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click()", ele);
+
+            IAlert prompt = driver.SwitchTo().Alert();
+
+            string alertText = prompt.Text;
+            Console.WriteLine("alert is text:" + alertText);
+
+            prompt.SendKeys("ok");
+
+            Thread.Sleep(2000);
+
+            prompt.Accept();
+
+            driver.Close();
+
+        }
+
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp2/Uploadfile.cs
@@ -0,0 +1,74 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp2
+{
+    class Uploadfile
+    {
+        IWebDriver driver;
+
+        [Test]
+
+        public void Upload()
+        {
+            driver = new ChromeDriver("D:\\auto\\chromedriver\\New folder");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            driver.Navigate().GoToUrl("http://demo.guru99.com/test/upload/");
+
+            Boolean boo = driver.FindElement(By.Id("terms")).Displayed;
+            Console.WriteLine("status audio box: " + boo);
+            Thread.Sleep(2000);
+
+            IWebElement upfile = driver.FindElement(By.Id("uploadfile_0"));
+            upfile.SendKeys("D:\\auto\\log\\Log1.txt");
+            Thread.Sleep(2000);
+
+            //Boolean boo = driver.FindElement(By.Id("terms")).Displayed;
+            //Console.WriteLine("status audio box: " + boo);
+            //Thread.Sleep(2000);
+
+            driver.FindElement(By.Id("terms")).Click();
+            Thread.Sleep(2000);
+
+            Boolean boo1 = driver.FindElement(By.Id("terms")).Displayed;
+            Console.WriteLine("status audio box: " + boo1);
+
+            IWebElement sub = driver.FindElement(By.Id("submitbutton")); sub.Click();
+            Thread.Sleep(2000);
+
+            //String text = "has been successfully uploaded.";
+            String text = "1 file";
+            string text1 = driver.FindElement(By.XPath("//*[@id='res']/center ")).Text;
+            Console.WriteLine("The expected  :" + text1);
+
+            if (text.Equals(text1))
+            {
+                Console.WriteLine("The expected heading is same as actual heading-- - :" + text1);
+            }
+            else
+                Console.WriteLine("The expected heading doesn't match the actual heading --- :" + text1);
+            //var t = Regex.Split(text.Text, "\r\n")[1];
+
+            //if (text =='has been successfully uploaded.')
+            //{
+            //Console.WriteLine("case pass: " + text);
+            //}
+            //else Console.WriteLine("case failed: " + sub.Text);
+
+
+            driver.Close();
+
+        }
+
+    }
+}
