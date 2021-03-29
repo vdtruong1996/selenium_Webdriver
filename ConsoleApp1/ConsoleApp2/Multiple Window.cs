309f10d38b352d3ac1d9d2d786440861e4c4581f
truong.vo <truong.vo@CPU08-TRUONGVO>
2021-03-29 11:35:13 +0700

    fffffffffff

---
+++ ConsoleApp1/ConsoleApp2/Multiple Window.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Support;
+using System.Threading;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp2
+{
+    class Multiple_Window
+    {
+        IWebDriver driver;
+        //public object arguments;
+
+        [Test]
+        public void Window()
+        {
+            driver = new ChromeDriver("D:\\auto\\chromedriver\\New folder");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            driver.Navigate().GoToUrl("https://demoqa.com/browser-windows");
+
+            //IWebElement ele = driver.FindElement(By.XPath("//*[@id='promtButton']"));
+            string WindowHandle = driver.CurrentWindowHandle;
+            Console.WriteLine("window is handle: " + WindowHandle);
+
+            IWebElement ele = driver.FindElement(By.Id("windowButton"));
+
+            //IWebElement clickElement = driver.FindElement(By.Id("button1"));
+
+            // Multiple click to open multiple window
+            for (var i = 0; i < 3; i++)
+            {
+                ele.Click();
+                Thread.Sleep(3000);
+            }
+
+            IWebElement ele1 = driver.FindElement(By.Id("messageWindowButton")); ele1.Click();
+            IAlert abc = driver.SwitchTo().Alert();
+            string al = abc.Text;
+            Console.WriteLine("text is a:" + al);
+
+            abc.Dismiss();
+
+            driver.Quit();
+
+        }
+    }
+}
