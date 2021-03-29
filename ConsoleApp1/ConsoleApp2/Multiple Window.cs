031841275f56e26cd16f998f4848de784cccdaf7
truong.vo <truong.vo@CPU08-TRUONGVO>
2021-03-29 14:07:51 +0700

    cvb

--- ConsoleApp1/ConsoleApp2/Multiple Window.cs
+++ ConsoleApp1/ConsoleApp2/Multiple Window.cs
@@ -26,6 +26,8 @@ namespace ConsoleApp2
 
             driver.Navigate().GoToUrl("https://demoqa.com/browser-windows");
 
+            //IWebElement elet = ("https://demoqa.com/browser-windows");
+
             //IWebElement ele = driver.FindElement(By.XPath("//*[@id='promtButton']"));
             string WindowHandle = driver.CurrentWindowHandle;
             Console.WriteLine("window is handle: " + WindowHandle);
@@ -41,12 +43,25 @@ namespace ConsoleApp2
                 Thread.Sleep(3000);
             }
 
-            IWebElement ele1 = driver.FindElement(By.Id("messageWindowButton")); ele1.Click();
-            IAlert abc = driver.SwitchTo().Alert();
-            string al = abc.Text;
-            Console.WriteLine("text is a:" + al);
+            List<string> lstWindow = driver.WindowHandles.ToList();
+            foreach (var handle in lstWindow)
+            {
+                Console.WriteLine("Switching to window - > " + handle);
+                Console.WriteLine("Navigating to google.com");
+                Thread.Sleep(2000);
 
-            abc.Dismiss();
+                driver.SwitchTo().Window(handle);
+                Thread.Sleep(2000);
+                driver.Navigate().GoToUrl("https://google.com");
+                Thread.Sleep(2000);
+            }
+
+            //IWebElement ele1 = driver.FindElement(By.Id("messageWindowButton")); ele1.Click();
+            //IAlert abc = driver.SwitchTo().Alert();
+            //string al = abc.Text;
+            //Console.WriteLine("text is a:" + al);
+
+            //abc.Dismiss();
 
             driver.Quit();
 
